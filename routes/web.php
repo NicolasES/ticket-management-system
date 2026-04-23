f37eff94a1cae90f11b5945fe8ae117250d26d59
NicolasES <nicolas.escouto@gmail.com>
2026-04-23 19:09:38 -0300

    feat: restore default welcome and dashboard routes and move simulation route to /simulate

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,16 @@ use App\Http\Controllers\SimulateSystemController;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
-// Route::inertia('/', 'welcome', [
-//     'canRegister' => Features::enabled(Features::registration()),
-// ])->name('home');
+Route::inertia('/', 'welcome', [
+    'canRegister' => Features::enabled(Features::registration()),
+])->name('home');
 
-// Route::middleware(['auth', 'verified'])->group(function () {
-//     Route::inertia('dashboard', 'dashboard')->name('dashboard');
-// });
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::inertia('dashboard', 'dashboard')->name('dashboard');
+});
 
 
-Route::get('/', SimulateSystemController::class)->name('simulate');
+// Route::get('/', SimulateSystemController::class)->name('home');
 Route::get('/simulate', SimulateSystemController::class)->name('simulate');
 
 
